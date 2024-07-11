32031833ea18ce76e69ba1ab6979c38db69f345b
Erayaltilar <eray.altilar@hotmail.com>
2024-07-11 20:36:00 +0300

    Create Food service and models

---
+++ app/src/main/java/com/example/android_training/data/remote/dto/FoodDto.kt
@@ -0,0 +1,6 @@
+package com.example.android_training.data.remote.dto
+
+data class FoodDto(
+    val id: String? = null,
+    val description: String? = null,
+)
---
+++ app/src/main/java/com/example/android_training/domain/model/Food.kt
@@ -0,0 +1,6 @@
+package com.example.android_training.domain.model
+
+data class Food(
+    val id: String? = null,
+    val description: String? = null,
+)
