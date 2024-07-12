6fa94d2e499c36f98077df1178e9beda9c009124
Erayaltilar <eray.altilar@hotmail.com>
2024-07-12 17:25:01 +0300

    Clear unused Api and models

--- app/src/main/java/com/example/android_training/data/remote/dto/FoodDto.kt
+++
@@ -1,6 +0,0 @@
-package com.example.android_training.data.remote.dto
-
-data class FoodDto(
-    val id: String? = null,
-    val description: String? = null,
-)
--- app/src/main/java/com/example/android_training/domain/model/Food.kt
+++
@@ -1,6 +0,0 @@
-package com.example.android_training.domain.model
-
-data class Food(
-    val id: String? = null,
-    val description: String? = null,
-)
