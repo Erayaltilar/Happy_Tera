d58455f63b1bfbd4a4786a96a7b0ce921b184ca1
Erayaltilar <eray.altilar@hotmail.com>
2024-07-12 17:28:23 +0300

    Create logic for GetRandomMessage from api

--- app/src/main/java/com/example/android_training/presentation/homepage/HomepageViewModel.kt
+++ app/src/main/java/com/example/android_training/presentation/homepage/HomepageViewModel.kt
@@ -1,13 +1,10 @@
 package com.example.android_training.presentation.homepage
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android_training.core.Resource
-import com.example.android_training.domain.model.Food
-import com.example.android_training.domain.usecase.GetAllFoodsUseCase
-import com.example.android_training.domain.usecase.GetFoodByIdUseCase
-import com.example.android_training.domain.usecase.GetFoodByNameUseCase
+import com.example.android_training.domain.model.Message
+import com.example.android_training.domain.usecase.GetRandomMessageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,27 +12,22 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
-import retrofit2.http.Query
 import javax.inject.Inject
 
 @HiltViewModel
 class HomepageViewModel @Inject constructor(
-    private val getAllFoodsUseCase: GetAllFoodsUseCase,
-    private val getFoodByIdUseCase: GetFoodByIdUseCase,
-    private val getFoodByNameUseCase: GetFoodByNameUseCase,
+    private val getRandomMessageUseCase: GetRandomMessageUseCase,
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(HomepageScreenUIState())
     val uiState: StateFlow<HomepageScreenUIState> = _uiState.asStateFlow()
 
     init {
-        //getFoodList()
-        //getFoodById()
-        //getFoodByName()
+        getRandomMessage()
     }
 
-    private fun getFoodList() {
-        getAllFoodsUseCase().onEach {
+    fun getRandomMessage() {
+        getRandomMessageUseCase().onEach {
             when (it) {
                 is Resource.Loading -> {
                     _uiState.update { state ->
@@ -53,7 +45,7 @@ class HomepageViewModel @Inject constructor(
                             loadingState = false,
                             isHaveError = false,
                             isSuccess = true,
-                            foodList = it.data ?: emptyList(),
+                            randomMessage = it.data,
                         )
                     }
                 }
@@ -63,81 +55,6 @@ class HomepageViewModel @Inject constructor(
                         state.copy(
                             loadingState = false,
                             isHaveError = true,
-                            errorMessage = it.message.orEmpty(),
-                        )
-                    }
-                }
-            }
-        }.launchIn(viewModelScope)
-    }
-
-    private fun getFoodById(id: String = "27906327547693") {
-        getFoodByIdUseCase(id).onEach {
-            when (it) {
-                is Resource.Loading -> {
-                    _uiState.update { state ->
-                        state.copy(
-                            loadingState = true,
-                            isHaveError = false,
-                            isSuccess = false,
-                        )
-                    }
-                }
-
-                is Resource.Success -> {
-                    _uiState.update { state ->
-                        state.copy(
-                            loadingState = false,
-                            isHaveError = false,
-                            isSuccess = true,
-                            food = it.data,
-                        )
-                    }
-                }
-
-                is Resource.Error -> {
-                    _uiState.update { state ->
-                        state.copy(
-                            loadingState = false,
-                            isHaveError = true,
-                            errorMessage = it.message.orEmpty(),
-                        )
-                    }
-                }
-            }
-        }.launchIn(viewModelScope)
-    }
-
-    fun getFoodByName(query: String) {
-        getFoodByNameUseCase(query).onEach {
-            when (it) {
-                is Resource.Loading -> {
-                    _uiState.update { state ->
-                        state.copy(
-                            isSearchLoading = true,
-                            isSearchError = false,
-                            isSearchSuccess = false,
-                        )
-                    }
-                }
-
-                is Resource.Success -> {
-                    _uiState.update { state ->
-                        state.copy(
-                            isSearchLoading = false,
-                            isSearchError = false,
-                            isSearchSuccess = true,
-                            foodList = it.data ?: emptyList(),
-                        )
-                    }
-                }
-
-                is Resource.Error -> {
-                    _uiState.update { state ->
-                        state.copy(
-                            isSearchLoading = false,
-                            isSearchError = true,
-                            errorMessage = it.message.orEmpty(),
                         )
                     }
                 }
@@ -150,10 +67,6 @@ class HomepageViewModel @Inject constructor(
         val isHaveError: Boolean = false,
         val isSuccess: Boolean = false,
         val errorMessage: String = "",
-        val foodList: List<Food> = emptyList(),
-        val isSearchLoading: Boolean = false,
-        val isSearchSuccess : Boolean = false,
-        val isSearchError: Boolean = false,
-        val food: Food? = null,
+        val randomMessage: Message? = null,
     )
 }
