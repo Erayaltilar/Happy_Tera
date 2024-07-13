db9c4970edba0bc82f5b5b3365aec6c6b5bef7ff
Erayaltilar <eray.altilar@hotmail.com>
2024-07-13 19:00:38 +0300

    Create a extention for activity navigation

---
+++ app/src/main/java/com/example/android_training/core/ContextExtention.kt
@@ -0,0 +1,10 @@
+package com.example.android_training.core
+
+import android.app.Activity
+import android.content.Intent
+
+fun Activity.goToTheActivity(activityToGo: Activity, isFinish: Boolean = false) {
+    val intent = Intent(this, activityToGo::class.java)
+    startActivity(intent)
+    if (isFinish) finish()
+}
