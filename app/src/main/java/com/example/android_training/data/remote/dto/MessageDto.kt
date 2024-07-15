74ab1db90559034d35a9b6eb7dd6d10e275b3d5f
Erayaltilar <eray.altilar@hotmail.com>
2024-07-15 15:12:13 +0300

    Change Direction of Advice message model and dto

--- app/src/main/java/com/example/android_training/data/remote/dto/MessageDto.kt
+++
@@ -1,5 +0,0 @@
-package com.example.android_training.data.remote.dto
-
-data class MessageDto(
-    val slip: AdviceDto,
-)
--- app/src/main/java/com/example/android_training/domain/model/Message.kt
+++
@@ -1,5 +0,0 @@
-package com.example.android_training.domain.model
-
-data class Message(
-    val slip: Advice,
-)
