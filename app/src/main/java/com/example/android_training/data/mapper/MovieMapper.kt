6b5cfb8d11482192019c4b63a53763bd87a37eee
Erayaltilar <eray.altilar@hotmail.com>
2024-07-18 21:57:18 +0300

    Change Mappers to fit to new API

--- app/src/main/java/com/example/android_training/data/mapper/MovieMapper.kt
+++ app/src/main/java/com/example/android_training/data/mapper/MovieMapper.kt
@@ -1,17 +1,20 @@
 package com.example.android_training.data.mapper
 
+import com.example.android_training.data.remote.dto.movie.GenresDto
 import com.example.android_training.data.remote.dto.movie.MovieDto
 import com.example.android_training.data.remote.dto.movie.MovieRequestDto
+import com.example.android_training.domain.model.movie_model.Genres
 import com.example.android_training.domain.model.movie_model.Movie
 import com.example.android_training.domain.model.movie_model.MovieRequest
 
 fun MovieDto.toMovie(): Movie {
     return Movie(
         id = id,
-        movie = movie,
-        rating = rating,
-        image = image,
-        imdb_url = imdb_url,
+        title = title,
+        overview = overview,
+        poster_path = poster_path,
+        vote_avarage = vote_avarage,
+        genres = genres?.map { it.toGenreList() }
     )
 }
 
@@ -19,9 +22,17 @@ fun List<MovieDto>.toMovieList(): List<Movie> {
     return map { it.toMovie() }
 }
 
-fun MovieRequestDto.toMovieRequest(): MovieRequest {
-    return MovieRequest(
-        success = success,
-        result = result.toMovieList(),
+fun GenresDto.toGenreList(): Genres {
+    return Genres(
+        id = id,
+        name = name,
     )
 }
+
+fun MovieRequestDto.toMovieRequest(): MovieRequest {
+    return MovieRequest(
+        page = page,
+        results = results.toMovieList(),
+    )
+}
+
